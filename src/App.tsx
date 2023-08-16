2f6878cc828a06bf2002ba85ee6076358bfbf2ae
Mustafa <gomushi@gmail.com>
2023-08-16 00:21:59 -0500

    okay working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import algoliasearch from "algoliasearch/lite";
+import {
+  Hits as AlgoliaHits,
+  Configure,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
+} from "react-instantsearch";
+
+import "instantsearch.css/themes/satellite.css";
+
+import Hits from "./components/Hits";
+
+// import "./App.css";
+import { Col, Layout, Row, Typography } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+
+const searchClient = algoliasearch(
+  "XP1F8B8ZH7",
+  process.env.REACT_APP_ALGOLIA_API_KEY!
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName="dev_hoda_airports"
+        insights
+      >
+        <Header style={{ backgroundColor: "gray", paddingTop: 10 }}>
+          <Row>
+            <Col span={8}>
+              <Typography.Title level={3}>
+                Hello Algolia Dev Interviewer 💻
+              </Typography.Title>
+            </Col>
+            <Col span={16}>
+              <SearchBox />
+            </Col>
+          </Row>
+        </Header>
+        <Layout
+          style={{
+            color: "whitesmoke",
+            fontFamily: "monospace",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Configure hitsPerPage={5} />
+
+          <Sider style={{ backgroundColor: "gray" }}>
+            <RefinementList attribute="country" />
+          </Sider>
+          <Content style={{ backgroundColor: "gray" }}>
+            <AlgoliaHits hitComponent={Hits} />
+            <Pagination />
+          </Content>
+        </Layout>
+      </InstantSearch>
     </div>
   );
 }
